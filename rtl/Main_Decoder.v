d7e6c8a66fb78770000a34de9bf6a76f77a5aecd
LaharB <laharbaruah96@gmail.com>
2026-04-16 17:20:14 +0530

    Main_Decoder logic updated

--- rtl/Main_Decoder.v
+++ rtl/Main_Decoder.v
@@ -1,36 +1,42 @@
 module Main_Decoder(
     input [6:0] op,
     input zero,
-    output RegWrite, MemWrite, ALUSrc, ResultSrc, PCSrc,
-    output [1:0] ImmSrc, ALUOp    
+    output RegWrite, MemWrite, ALUSrc, Branch, //PCSrc,
+    output [1:0] ImmSrc, ResultSrc, ALUOp    
     );
     
-    wire Branch;
-
     /*
-    -- Main decoder has been designed for 4 types of instructions : lw , sw , R-type and beq
-    i.e one of each I-type, S-type, R-type and B-type is demonstrated
+    -- Main decoder has been designed for 5 types of instructions : lw , sw , R-type, beq and addi
+    i.e each I-type, S-type, R-type and B-type is demonstrated
 
-    -- Note: We have assumed "dont care" values as 0 for simplicity
-    // =============================================================================================
-    // Main Control Unit Truth Table
-    // =============================================================================================
-    // Instruction |    Op     | RegWrite | ImmSrc | ALUSrc | MemWrite | ResultSrc | Branch | ALUOp
-    // ------------|-----------|----------|--------|--------|----------|-----------|--------|-------
-    // lw          | 0000011   |    1     |   00   |   1    |    0     |     1     |   0    |  00
-    // sw          | 0100011   |    0     |   01   |   1    |    1     |     x     |   0    |  00
-    // R-type      | 0110011   |    1     |   xx   |   0    |    0     |     0     |   0    |  10
-    // beq         | 1100011   |    0     |   10   |   0    |    0     |     x     |   1    |  01
-    // =============================================================================================
+    -- Note: We have assumed some "dont care" values as 0 for simplicity
+    // =======================================================================================================
+    // Modified Main Control Unit Truth Table (with Jump)
+    // =======================================================================================================
+    // Instruction | Opcode  | RegWrite | ImmSrc | ALUSrc | MemWrite | ResultSrc | Branch | ALUOp | Jump
+    // ------------|---------|----------|--------|--------|----------|-----------|--------|-------|------
+    // lw          | 0000011 |    1     |   00   |   1    |    0     |    01     |   0    |  00   |  0
+    // sw          | 0100011 |    0     |   01   |   1    |    1     |    xx     |   0    |  00   |  0
+    // R-type      | 0110011 |    1     |   xx   |   0    |    0     |    00     |   0    |  10   |  0
+    // beq         | 1100011 |    0     |   10   |   0    |    0     |    xx     |   1    |  01   |  0
+    // I-type ALU  | 0010011 |    1     |   00   |   1    |    0     |    00     |   0    |  10   |  0
+    // jal         | 1101111 |    1     |   11   |   x    |    0     |    10     |   0    |  xx   |  1
+    // =======================================================================================================
     */
-    assign RegWrite = ((op == 7'b0000011) || (op == 7'b0110011)) ? 1'b1 : 1'b0;
-    assign ImmSrc = (op == 7'b0100011) ? 2'b01 : (op == 7'b1100011) ? 2'b10 : 2'b00;
-    assign ALUSrc = ((op == 7'b0000011) || (op == 7'b0100011)) ? 1'b1 : 1'b0;
-    assign MemWrite = (op == 7'b0100011) ? 1'b1 : 1'b0;
-    assign ResultSrc = (op == 7'b0000011) ? 1'b1 : 1'b0;
-    assign Branch = (op == 7'b1100011) ? 1'b1 : 1'b0;
-    assign ALUOp = (op == 7'b0110011) ? 2'b10 : (op == 7'b1100011) ? 2'b01 : 2'b00;
+    assign RegWrite = ((op == 7'b0000011) || (op == 7'b0110011) || (op == 7'b0010011) || (op == 7'b1101111)) ? 1'b1 : 1'b0;
 
-    assign PCSrc = zero & Branch;
+    assign ImmSrc = (op == 7'b0100011) ? 2'b01 : (op == 7'b1100011) ? 2'b10 : ((op == 7'b0010011) || (op == 7'b0000011)) ? 2'b00 : (op == 1101111) ? 2'b11 : 2'bxx;   
+
+    assign ALUSrc = ((op == 7'b0000011) || (op == 7'b0100011) || (op == 7'b0010011)) ? 1'b1 : ((op == 7'b0110011) || (op == 7'b1100011) )? 1'b0 : 1bx;
+
+    assign MemWrite = (op == 7'b0100011) ? 1'b1 : 1'b0;
+
+    assign ResultSrc = (op == 7'b0000011) ? 2'b01 : ((op == 7'b0110011) || (op == 7'b0010011)) ? 2'b00 : (op == 7'b1101111) ? 2'10 : 2'bxx;
+
+    assign Branch = (op == 7'b1100011) ? 1'b1 : 1'b0;
+
+    assign ALUOp = ((op == 7'b0110011) || (op = 7'b0010011)) ? 2'b10 : (op == 7'b1100011) ? 2'b01 : ((op == 7'b0000011) || (op == 7'b0100011)) ? 2'b00 : 2'bxx;
+
+    //assign PCSrc = zero & Branch; 
   
 endmodule
