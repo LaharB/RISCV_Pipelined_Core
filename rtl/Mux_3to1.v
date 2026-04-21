745cb80f17074aa2ab9d49f4cf5ce3fe6462857c
LaharB <laharbaruah96@gmail.com>
2026-04-21 21:26:57 +0530

    Files Updated

---
+++ rtl/Mux_3to1.v
@@ -0,0 +1,15 @@
+module Mux_3to1(
+    input [31:0] a, b, c,
+    input [1:0] sel,
+    output reg [31:0] y
+);
+
+    case(sel)
+        2'b00 : y = a;
+        2'b01 : y = b;
+        2'b10 : y = c;
+        
+        default: y = 31'hxxxx_xxxx;   
+    endcase
+    
+endmodule
--- rtl/Writeback_Stage.v
+++ rtl/Writeback_Stage.v
@@ -1,9 +1,21 @@
-`include ""
-
-
+`include "Mux_3to1.v"
 
 module Writeback_Stage(
-    ports
+    //coming to MEMORY Stage
+    input [1:0] ResultSrcW,
+    input [31:0] ALUResultW, ReadDataW, PCPlus4W,
+
+    //going to DECODE and EXECUTE Stages
+    output [31:0] ResultW
 );
-    
+
+    //Mux_3x1 module
+    Mux_3to1 Mux_3to1(
+        .a(ALUResultW), 
+        .b(ReadDataW), 
+        .c(PCPlus4W),
+        .sel(ResultSrcW),
+        .y(RdW)
+    );
+
 endmodule
