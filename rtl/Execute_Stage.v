f74c82cd4cee86fcef3f682996c9723c13844914
LaharB <laharbaruah96@gmail.com>
2026-04-21 19:54:23 +0530

    Files Updated

--- rtl/Execute_Stage.v
+++ rtl/Execute_Stage.v
@@ -20,37 +20,87 @@ module Execute_Stage(
     output [4:0] RdM,
 
     //going to Execute Stage
+    output PCSrcE,
     output [31:0] PCTargetE   
 );
     //wires
+    wire SrcB_wire, ZeroE, CarryE, OverflowE, NegativeE;
+    wire [31:0] ALUResultE_wire;
+    wire and_to_or_wire; 
 
-    wire 
-
-
-    //and gate 
-    assign 
-
-    //or gate
-    aasign 
+    //EXECUTE Stage Registers
+    reg RegWriteE_reg, WriteMemE_reg;
+    reg [1:0] ResultSrcE_reg;
+    reg [31:0] ALUResultE_reg, RD2E_reg, PCPlus4E_reg;
+    reg [4:0] WriteDataE_reg; //to store RD2E value 
 
     //Mux_2to1 module
     Mux_2to1 Mux_SrcBE(
-        .a(), 
-        .b(),
-        .sel(),
-        .y()
+        .a(RD2E), 
+        .b(ImmExtE),
+        .sel(ALUSrcE),
+        .y(SrcB_wire)
     );
 
     //PC_Adder module
     PC_Adder PCTargetE(
-        .a(), 
-        .b(),
-        .c()
+        .a(PCE), 
+        .b(ImmExtE),
+        .c(PCTargetE)
     );
 
-    //
+    //ALU module 
+    ALU ALU(
+        .SrcA(RD1E),
+        .SrcB(SrcB_wire),
+	    .ALUControl(ALUControlE), 
+	    .Carry(CarryE), 
+        .Overflow(OverflowE), 
+        .Zero(ZeroE), 
+        .Negative(NegativeE),
+	    .result(ALUResultE_wire)
+    );
 
-
-
+    //Storing values inside EXECUTE Registers
+    always@(posedge clk or negedge rst) begin
+        if(!rst) begin
+            RegWriteE_reg <= 1'b0; 
+            WriteMemE_reg <= 1'b0;
+            ResultSrcE_reg <= 2'b00;
+            ALUResultE_reg <= 32'h0000_0000; 
+            RD2E_reg <= 32'h0000_0000; 
+            PCPlus4E_reg <= 32'h0000_0000;
+            RdE_reg <= 5'h00;
+        end 
+        else begin
+            RegWriteE_reg <= RegWriteE; 
+            WriteMemE_reg <= WriteMemE;
+            ResultSrcE_reg <= ResultSrcE;
+            ALUResultE_reg <= ALUResultE; 
+            WriteDataE_reg <= RD2E; 
+            PCPlus4E_reg <= PCPlus4E;
+            RdE_reg <= RdE;
+        end
+    end
     
+    reg RegWriteE_reg, WriteMemE_reg;
+    reg [1:0] ResultSrcE_reg;
+    reg [31:0] ALUResultE_reg, RD2E_reg, PCPlus4E_reg,
+    reg [4:0] RdE_reg,
+
+    //and gate 
+    assign and_to_or_wire = ZeroE & BranchE;
+
+    //or gate
+    assign PCSrcE = JumpE | and_to_or_wire; 
+
+    //Passing values from EXECUTE Registers to WRITEBACK Stage
+    assign RegWriteM = RegWriteE_reg;
+    assign WriteMemM = WriteMemE_reg;
+    assign ResultSrcM = ResultSrcE_reg;
+    assign ALUResultM = ALUResultE_reg;
+    assign WriteDataM = WriteDataE_reg;
+    assign PCPlus4M = PCPlus4E_reg;
+    assign RdM = RdE_reg;
+  
 endmodule
