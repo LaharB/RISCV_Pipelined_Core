d2a5ac458cb4a241081bb476f2df91acd25fa421
LaharB <laharbaruah96@gmail.com>
2026-04-26 00:44:28 +0530

    Files Updated

--- rtl/Hazard_Unit.v
+++ rtl/Hazard_Unit.v
@@ -4,6 +4,20 @@ module Hazard_Unit(
     output [1:0] ForwardAE, ForwardBE
 );
 
+/*
+ * ==================================================================================================================
+ * | Mux control   | Source   | Explanation                                                                         |
+ * |===============|==========|=====================================================================================|
+ * | ForwardA = 00 | ID/EX    | The first ALU operand comes from the register file.                                 |
+ * | ForwardA = 10 | EX/MEM   | The first ALU operand is forwarded from the prior ALU result.                       |
+ * | ForwardA = 01 | MEM/WB   | The first ALU operand is forwarded from data memory or an earlier ALU result.       |
+ * |---------------|----------|-------------------------------------------------------------------------------------|
+ * | ForwardB = 00 | ID/EX    | The second ALU operand comes from the register file.                                |
+ * | ForwardB = 10 | EX/MEM   | The second ALU operand is forwarded from the prior ALU result.                      |
+ * | ForwardB = 01 | MEM/WB   | The second ALU operand is forwarded from data memory or an earlier ALU result.      |
+ * ==================================================================================================================
+ */
+ 
     assign ForwardAE = (!rst)? 2'b00 : 
                        ((RegWriteM == 1'b1) && (RdM != 5'h00) && (RdM == Rs1E)) ? 2'b10 : 
                        ((RegWriteW == 1'b1) && (RdW != 5'h00) && (RdW == Rs1E)) ? 2'b01 : 2'b00;
