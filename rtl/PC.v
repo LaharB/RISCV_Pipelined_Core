f4d3adff43055099296775436f4f49da96decff2
LaharB <laharbaruah96@gmail.com>
2026-05-21 14:25:00 +0530

    Files Updated

--- rtl/PC.v
+++ rtl/PC.v
@@ -1,5 +1,7 @@
 module PC(
     input clk, rst,
+    //for Hazard Unit - Stalling 
+    input en, //active low enable 
     input [31:0] PC_Next,
     output reg [31:0] PC
 );
@@ -7,8 +9,8 @@ module PC(
     always@(posedge clk)
         begin
             if(!rst)
-                PC <= 32'h0000_0000;
-            else
+                PC <= 32'h0000_0000; 
+            else if(!en) 
                 PC <= PC_Next;    
         end
     
